19980ff4355c8d3355848186e3dd959eb086f850
smartmic <>
2025-12-05 20:17:08 +0000

    Expand parsing and Janet wrapping for all VEVENT properties
    FossilOrigin-Name: 0ac1db00c1cbd2ad08cf4ddda46d3e6dda015c5ec8f92790f91b686edb09dce9

--- ical.c
+++ ical.c
@@ -61,6 +61,108 @@ static Janet cfun_table_from_ics(int32_t argc, Janet *argv) {
             case ICAL_DTSTAMP_PROPERTY:
               janet_table_put(event, janet_cstringv("dtstamp"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_dtstamp(prop))));
               break;
+            case ICAL_DTSTART_PROPERTY:
+              janet_table_put(event, janet_cstringv("dtstart"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_dtstart(prop))));
+              break;
+            case ICAL_CLASS_PROPERTY:
+              janet_table_put(event, janet_cstringv("class"), janet_cstringv(icalproperty_enum_to_string(icalproperty_get_class(prop))));
+              break;
+            case ICAL_CREATED_PROPERTY:
+              janet_table_put(event, janet_cstringv("created"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_dtstamp(prop))));
+              break;
+            case ICAL_DESCRIPTION_PROPERTY:
+              janet_table_put(event, janet_cstringv("description"), janet_cstringv(icalproperty_get_description(prop))); 
+              break;
+            case ICAL_GEO_PROPERTY:
+              struct icalgeotype geo;
+              geo = icalproperty_get_geo(prop);
+              JanetTable *coordinates = janet_table(2);
+              janet_table_put(coordinates, janet_cstringv("lon"), janet_wrap_number(geo.lon));
+              janet_table_put(coordinates, janet_cstringv("lat"), janet_wrap_number(geo.lat));
+              janet_table_put(event, janet_cstringv("geo"), janet_wrap_struct(janet_table_to_struct(coordinates)));
+              break;
+            case ICAL_LASTMODIFIED_PROPERTY:
+              janet_table_put(event, janet_cstringv("last-mod"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_lastmodified(prop))));
+              break;
+            case ICAL_LOCATION_PROPERTY:
+              janet_table_put(event, janet_cstringv("location"),  janet_cstringv(icalproperty_get_location(prop)));
+              break;
+            case ICAL_ORGANIZER_PROPERTY:
+              janet_table_put(event, janet_cstringv("organizer"), janet_cstringv(icalproperty_get_organizer(prop)));
+              break;
+            case ICAL_PRIORITY_PROPERTY:
+              janet_table_put(event, janet_cstringv("priority"), janet_wrap_integer(icalproperty_get_priority(prop)));
+              break;
+            case ICAL_SEQUENCE_PROPERTY:
+              janet_table_put(event, janet_cstringv("seq"), janet_wrap_integer(icalproperty_get_sequence(prop)));
+              break;
+            case ICAL_STATUS_PROPERTY:
+              janet_table_put(event, janet_cstringv("status"),  janet_cstringv(icalproperty_enum_to_string(icalproperty_get_status(prop))));
+              break;
+            case ICAL_SUMMARY_PROPERTY:
+              janet_table_put(event, janet_cstringv("summary"), janet_cstringv(icalproperty_get_summary(prop)));
+              break;
+            case ICAL_TRANSP_PROPERTY:
+              janet_table_put(event, janet_cstringv("transp"), janet_cstringv(icalproperty_enum_to_string(icalproperty_get_transp(prop))));
+              break;
+            case ICAL_URL_PROPERTY:
+              janet_table_put(event, janet_cstringv("url"), janet_cstringv(icalproperty_get_url(prop)));
+              break;
+            case ICAL_RECURRENCEID_PROPERTY:
+              janet_table_put(event, janet_cstringv("recurid"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_recurrenceid(prop))));
+              break;
+            case ICAL_RRULE_PROPERTY:
+              janet_table_put(event, janet_cstringv("url"), janet_cstringv(icalproperty_as_ical_string(prop)));
+              break;
+            case ICAL_DTEND_PROPERTY:
+              janet_table_put(event, janet_cstringv("dtend"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_dtend(prop))));
+              break;
+            case ICAL_DURATION_PROPERTY:
+              struct icaldurationtype dur;
+              dur = icalproperty_get_duration(prop);
+              JanetTable *duration = janet_table(6);
+              janet_table_put(duration, janet_cstringv("days"), janet_wrap_integer(dur.days));
+              janet_table_put(duration, janet_cstringv("hours"), janet_wrap_integer(dur.hours));
+              janet_table_put(duration, janet_cstringv("is_neg"), janet_wrap_integer(dur.is_neg));
+              janet_table_put(duration, janet_cstringv("minutes"), janet_wrap_number(dur.minutes));
+              janet_table_put(duration, janet_cstringv("seconds"), janet_wrap_number(dur.seconds));
+              janet_table_put(duration, janet_cstringv("weeks"), janet_wrap_number(dur.weeks));
+              janet_table_put(event, janet_cstringv("duration"), janet_wrap_struct(janet_table_to_struct(duration)));
+              break;
+            case ICAL_ATTACH_PROPERTY:
+              if (icalattach_get_is_url(icalproperty_get_attach(prop))) {
+                janet_table_put(event, janet_cstringv("attach"), janet_cstringv(icalattach_get_url(icalproperty_get_attach(prop))));
+              } else {
+                janet_table_put(event, janet_cstringv("attach"), janet_cstringv(icalattach_get_data(icalproperty_get_attach(prop))));
+              }
+              break;
+            case ICAL_ATTENDEE_PROPERTY:
+              janet_table_put(event, janet_cstringv("attendee"), janet_cstringv(icalproperty_get_attendee(prop)));
+              break;
+            case ICAL_CATEGORIES_PROPERTY:
+              janet_table_put(event, janet_cstringv("categories"),  janet_cstringv(icalproperty_get_categories(prop)));
+              break;
+            case ICAL_COMMENT_PROPERTY:
+              janet_table_put(event, janet_cstringv("comment"), janet_cstringv(icalproperty_get_comment(prop)));
+              break;
+            case ICAL_CONTACT_PROPERTY:
+              janet_table_put(event, janet_cstringv("contact"),  janet_cstringv(icalproperty_get_contact(prop)));
+              break;
+            case ICAL_EXDATE_PROPERTY:
+              janet_table_put(event, janet_cstringv("exdate"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_exdate(prop))));
+              break;
+            case ICAL_REQUESTSTATUS_PROPERTY:
+              janet_table_put(event, janet_cstringv("rstatus"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_dtstamp(prop))));
+              break;
+            case ICAL_RELATEDTO_PROPERTY:
+              janet_table_put(event, janet_cstringv("related"), janet_cstringv(icalproperty_get_relatedto(prop)));
+              break;
+            case ICAL_RESOURCES_PROPERTY:
+              janet_table_put(event, janet_cstringv("resources"), janet_cstringv(icalproperty_get_resources(prop)));
+              break;
+            case ICAL_RDATE_PROPERTY:               
+              janet_table_put(event, janet_cstringv("rdate"), janet_wrap_integer(icaltime_as_timet(icalproperty_get_dtstamp(prop))));
+              break;
             default:
               
             }
--- test/table-from-ics.janet
+++ test/table-from-ics.janet
@@ -1,10 +1,18 @@
 (use judge)
 (import ical)
 
-#(test (file/read stdin :all) @"BEGIN:VCALENDAR\nMETHOD:REQUEST\nPRODID: -//hacksw/handcal//NONSGML v1.0//EN\nBEGIN:VEVENT\nDTSTAMP:19980309T231000Z\nUID:guid-1.host1.com\nORGANIZER;ROLE=CHAIR:MAILTO:mrbig@host.com\nATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:\n  MAILTO:employee-A@host.com\nDESCRIPTION:Project XYZ Review Meeting\nCATEGORIES:MEETING\nCLASS:PUBLIC\nCREATED:19980309T130000Z\nSUMMARY:XYZ Project Review\nDTSTART;TZID=US-Eastern:19980312T083000\nDTEND;TZID=US-Eastern:19980312T093000\nLOCATION:1CP Conference Room 4350\nEND:VEVENT\nEND:VCALENDAR\n\n")
-
 (test (ical/table_from_ics (file/read stdin :all))
-  @{"events" @[@{"dtstamp" 889485000
+  @{"events" @[@{"attendee" "MAILTO:employee-A@host.com"
+                 "categories" "MEETING"
+                 "class" "PUBLIC"
+                 "created" 889448400
+                 "description" "Project XYZ Review Meeting"
+                 "dtend" 889695000
+                 "dtstamp" 889485000
+                 "dtstart" 889691400
+                 "location" "1CP Conference Room 4350"
+                 "organizer" "MAILTO:mrbig@host.com"
+                 "summary" "XYZ Project Review"
                  "uid" "guid-1.host1.com"}]
     "prodid" "-//hacksw/handcal//NONSGML v1.0//EN"
     "version" "1.0"})
