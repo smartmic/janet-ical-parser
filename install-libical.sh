55c5ec00e44037a8a78165ad7ffb6678e1b56066
smartmic <>
2026-01-05 14:01:44 +0000

    Fix missing: Add shell script for local libical installation
    FossilOrigin-Name: aa2eadc55708c434f1f0a79ed356e88d689b1bcd04fd4c7c9b0aa048c5fe733b

---
+++ install-libical.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu   # Exit on errors and undefined variables
+ 
+ROOT_PATH=$(pwd)
+
+cd libical
+
+mkdir build && cd build
+
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${ROOT_PATH}/jpm_tree \
+    -DUSE_BUILTIN_TZDATA=TRUE \
+    -DICAL_BUILD_DOCS=FALSE \
+    -DICAL_GLIB=FALSE \
+    -DLIBICAL_BUILD_TESTING=FALSE \
+    -DWITH_CXX_BINDINGS=FALSE \
+    ..
+
+make
+make install
